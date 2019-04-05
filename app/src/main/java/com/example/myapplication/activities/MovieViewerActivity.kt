25998a12e1d66b086a44da99819dcf2c64ec51c2
d1egopolanco <00301417@uca.edu.sv>
2019-04-05 15:35:44 -0600

    commit

---
+++ app/src/main/java/com/example/myapplication/activities/MovieViewerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class MovieViewerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.viewer_movie)
+    }
+}
